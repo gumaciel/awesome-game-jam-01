d0bb8c1b606ba701793b7899090c3e3a9116cce6
Gustavo Maciel <gustamaciel2012@hotmail.com>
2023-09-05 20:18:19 -0300

    refactor: classes not inheriting player_base.gd

--- characters/Player_2/player_2.gd
+++
@@ -1,9 +0,0 @@
-extends PlayerBase
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	self.set_physics_process(false)
-	pass # Replace with function body.
-
-
--- characters/player_base.gd
+++ characters/player_base.gd
@@ -1,15 +1,23 @@
+class_name PlayerBase
 extends CharacterBody2D
-class_name  PlayerBase
 
 var direction : Vector2
 var SPEED : float = 200
 
+@export var active := true :
+	set(value):
+		set_physics_process(value)
+		set_process(value)
+		set_process_unhandled_input(value)
+		active = value
+
+func _ready():
+	self.active = active
 
 func _physics_process(delta: float) -> void:
 	direction.x = Input.get_axis("ui_left","ui_right")
 	direction.y = Input.get_axis("ui_up","ui_down")
 	_movement(SPEED)
-	pass
 
 func _movement(speed):
 	velocity = direction * speed
