e7acb80419a38c2ff07238a2decaee5ad2cdf44f
Gustavo Maciel <gustamaciel2012@hotmail.com>
2023-09-05 21:44:06 -0300

    refactor: clean code on player_template_1

--- characters/templates/player_template_1.gd
+++ characters/templates/player_template_1.gd
@@ -8,17 +8,16 @@ const GRID_SIZE: float = 16.0
 var is_moving: bool = false
 
 func _physics_process(_delta: float) -> void:
-	if (
-		Input.get_axis("ui_left", "ui_right") != 0 or 
-		Input.get_axis("ui_up", "ui_down") != 0
-	) and is_moving == false:
+	if get_direction() != Vector2.ZERO and not is_moving:
 		is_moving = true
 		var direction: Vector2 = get_direction()
 		var move_tween = create_tween()
-		move_tween.tween_property(self, 
-		"position", 
-		position + direction * GRID_SIZE, 
-		0.5)
+		move_tween.tween_property(
+			self, 
+			"position", 
+			position + direction * GRID_SIZE, 
+			0.5
+		)
 		await get_tree().create_timer(.5).timeout
 		is_moving = false
 
