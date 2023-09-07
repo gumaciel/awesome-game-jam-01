8a75984695145592b268d43cce2e496304adeaa5
Gustavo Maciel <gustamaciel2012@hotmail.com>
2023-09-07 19:47:34 -0300

    feat: FMOD working for Desktop and Mobile

--- scenes/fmod/banco_song.gd
+++ scenes/fmod/banco_song.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 @export var bank_asset: BankAsset
+@export var event: EventAsset
+
 var bank: Bank
 
 func _enter_tree() -> void:
 	bank = FMODStudioModule.get_studio_system().load_bank_file(bank_asset.file_path, FMODStudioModule.FMOD_STUDIO_LOAD_BANK_NORMAL, false)
+	RuntimeManager.play_one_shot(event)
