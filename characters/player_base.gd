c6363746841a8abb6f0e8ddee2aab055188e7977
Gustavo Maciel <gustamaciel2012@hotmail.com>
2023-09-08 18:34:22 -0300

    fix: colisão com espinhos funcionando
    - a colisão so acontece se mover depois que o jogador estiver em cima do bloco

--- characters/player_base.gd
+++ characters/player_base.gd
@@ -21,15 +21,5 @@ func _ready():
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("colidiu")
-	pass # Replace with function body.
+	print("_on_area_2d_body_entered")
 
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	
-	pass # Replace with function body.
-
-
-func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
-	print("colidiu")
-	pass # Replace with function body.
