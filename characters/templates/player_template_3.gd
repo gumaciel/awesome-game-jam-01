d09698c6f8d20d18a8198d080cf3c8bac70df849
FallenAngelDev <87909641+FallenAngelDev@users.noreply.github.com>
2023-09-07 22:29:53 -0300

    Dev/Fix Colisão implementada
    Por enquanto a colisão está assim, e preciso melhorar a movimentação. troquei os sprites dos personagens um pelo outro

--- characters/templates/player_template_3.gd
+++ characters/templates/player_template_3.gd
@@ -1,5 +1,6 @@
-extends CharacterBody2D
+extends Node2D
 
+var raycasts : Node2D
 var inputs = {  "ui_up"    : Vector2.UP,
 				"ui_down"  : Vector2.DOWN,
 				"ui_left"  : Vector2.LEFT,
@@ -7,12 +8,21 @@ var inputs = {  "ui_up"    : Vector2.UP,
 			}
 
 func _unhandled_input(event) -> void:
+	var colliders : Array = []
+	for ray in raycasts.get_children():
+		if ray.is_colliding():
+			colliders.append(ray.target_position.clamp(Vector2(-1,-1),Vector2(1,1)))
+
 	for dir in inputs.keys():
 		if event.is_action_pressed(dir):
+			if colliders.has(inputs[dir]):
+				break
 			move(dir)
+
 
 func move(dir) -> void:
 	var move_tween = create_tween()
 	var tile_position = position + inputs[dir] * 16
 	move_tween.tween_property(self, "position",tile_position , 0.5 )
-	
+
+
