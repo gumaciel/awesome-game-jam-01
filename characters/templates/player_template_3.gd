e47440d2fe9068b9c480c96cfcaef881a843f96c
FallenAngelDev <87909641+FallenAngelDev@users.noreply.github.com>
2023-09-08 00:01:56 -0300

    uns ajustes

--- characters/templates/player_template_3.gd
+++ characters/templates/player_template_3.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var is_walking = false
 var raycasts : Node2D
 var inputs = {  "ui_up"    : Vector2.UP,
 				"ui_down"  : Vector2.DOWN,
@@ -7,22 +8,30 @@ var inputs = {  "ui_up"    : Vector2.UP,
 				"ui_right" : Vector2.RIGHT
 			}
 
+
 func _unhandled_input(event) -> void:
+
+	if is_walking == false:
+		for dir in inputs.keys():
+			if event.is_action_pressed(dir):
+				move(dir)
+				
+		
+
+
+
+func move(dir) -> void:
+	is_walking = true
 	var colliders : Array = []
 	for ray in raycasts.get_children():
 		if ray.is_colliding():
 			colliders.append(ray.target_position.clamp(Vector2(-1,-1),Vector2(1,1)))
-
-	for dir in inputs.keys():
-		if event.is_action_pressed(dir):
-			if colliders.has(inputs[dir]):
-				break
-			move(dir)
-
-
-func move(dir) -> void:
+	if colliders.has(inputs[dir]):
+		is_walking = false
+		return
 	var move_tween = create_tween()
 	var tile_position = position + inputs[dir] * 16
 	move_tween.tween_property(self, "position",tile_position , 0.5 )
+	is_walking = false
 
 
