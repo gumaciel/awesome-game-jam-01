acbb07d1c784c757e8ffa45c81a12336db2ba8c1
FallenAngelDev <87909641+FallenAngelDev@users.noreply.github.com>
2023-09-08 22:22:13 -0300

    mais ajustes

--- world/tilesets_data/scenario_environment.gd
+++ world/tilesets_data/scenario_environment.gd
@@ -6,21 +6,6 @@ var spikes = Vector2i(2,1)
 var bridges = [Vector2i(2,3),Vector2i(3,3),Vector2i(4,3),Vector2i(5,3)]
 var hole = Vector2i(6,0)
 
-func _physics_process(delta: float) -> void:
-	pass
-#	var tile_pos = self.local_to_map(PlayerManagger.current_player_position)
-#	var current_tile = self.get_cell_atlas_coords(0,tile_pos)
-#	if current_tile == spikes_holes:
-#		_spikes(tile_pos)
-#		return
-#	if bridges.has(current_tile):
-#		_bridge(tile_pos)
-#		return
-#	if current_tile == spikes:
-#		print("game over")
-
-
-
 
 func _spikes(tile_pos) -> void:
 	var timer = get_tree().create_timer(0.5)
