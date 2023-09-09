761cf1271a9461393712062471a6d702a845e55e
Gustavo Maciel <gustamaciel2012@hotmail.com>
2023-09-08 23:48:30 -0300

    fix: background_color_rect not updating correctly

--- scenes/ui/background_color_rect.gd
+++ scenes/ui/background_color_rect.gd
@@ -1,9 +1,12 @@
 extends ColorRect
 
-func _ready():
+func _ready() -> void:
 	get_tree().root.size_changed.connect(_on_viewport_size_changed)
-	size = DisplayServer.screen_get_size()
+	_update_size()
+	
+func _on_viewport_size_changed() -> void:
+	_update_size()
 
-func _on_viewport_size_changed():
-	set_anchors_and_offsets_preset(Control.PRESET_CENTER, Control.PRESET_MODE_KEEP_SIZE)
+func _update_size() -> void:
 	size = DisplayServer.screen_get_size()
+	set_anchors_and_offsets_preset(Control.PRESET_CENTER, Control.PRESET_MODE_KEEP_SIZE)
