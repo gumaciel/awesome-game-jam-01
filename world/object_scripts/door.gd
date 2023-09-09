f10d24549347fec6c145ee3c8842dfd6137d52ef
FallenAngelDev <87909641+FallenAngelDev@users.noreply.github.com>
2023-09-09 14:51:33 -0300

    pequena alterações

--- world/object_scripts/door.gd
+++ world/object_scripts/door.gd
@@ -7,8 +7,10 @@ extends Node2D
 func _on_button_area_entered(area: Area2D) -> void:
 	door_spr.frame = 0
 	static_colision.set_deferred("disabled",true)
+	door_spr.z_index = 0
 
 
 func _on_button_area_exited(area: Area2D) -> void:
 	door_spr.frame = 1
 	static_colision.set_deferred("disabled",false)
+	door_spr.z_index = -1
