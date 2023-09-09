076d100f4569579dc40c97d45a368af699b04791
FallenAngelDev <87909641+FallenAngelDev@users.noreply.github.com>
2023-09-09 13:07:39 -0300

    dev desenvolvendo alguns obstáculos
    pontes e fire canons

--- characters/player_base.gd
+++ characters/player_base.gd
@@ -6,6 +6,7 @@ var direction : Vector2
 @export var speed_time : float
 @onready var base_raycasts : Node2D = $Raycasts
 @export var animations : AnimationPlayer
+@export var heavy : bool
 @export var active := true :
 	set(value):
 		set_physics_process(value)
@@ -18,8 +19,11 @@ func _ready():
 	move_time = speed_time
 	raycasts = base_raycasts
 	animation_player = animations
+	is_heavy = heavy
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("colidiu")
+	print("game over")
+
+
 
