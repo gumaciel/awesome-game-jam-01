3210d6e517a09ae38e8a289a341ae61e1609d83c
FallenAngelDev <87909641+FallenAngelDev@users.noreply.github.com>
2023-09-09 15:39:52 -0300

    alguns ajustes
    troca de sprite da porta e implementação do placeholder do canhão de flecas

--- world/object_scripts/door.gd
+++ world/object_scripts/door.gd
@@ -2,15 +2,16 @@ extends Node2D
 
 @onready var door_spr = $DoorSprite
 @onready var static_colision = $StaticBody2D/CollisionShape2D
+@onready var animation = $GateAnimation
 
 
 func _on_button_area_entered(area: Area2D) -> void:
-	door_spr.frame = 0
+	animation.play("open")
 	static_colision.set_deferred("disabled",true)
 	door_spr.z_index = 0
 
 
 func _on_button_area_exited(area: Area2D) -> void:
-	door_spr.frame = 1
+	animation.play_backwards("open")
 	static_colision.set_deferred("disabled",false)
 	door_spr.z_index = -1
