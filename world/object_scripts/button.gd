eae06432981f1a6c9929ec6301765eb80e09e8d2
Gustavo Maciel <gustamaciel2012@hotmail.com>
2023-09-10 14:08:42 -0300

    feat: hide buttons if not Mobile

--- world/object_scripts/button.gd
+++ world/object_scripts/button.gd
@@ -1,15 +1,7 @@
 extends Area2D
 
-
-
-func _on_area_entered(area: Area2D) -> void:
+func _on_area_entered(_area: Area2D) -> void:
 	$ButtonSprite.frame = 1
 
-	
-
-
-
-func _on_area_exited(area: Area2D) -> void:
+func _on_area_exited(_area: Area2D) -> void:
 	$ButtonSprite.frame = 0
-
-	
