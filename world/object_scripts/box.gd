64d37cec9376e1a565b57e8e8f80d3589255956d
Marcos Sousa <104542682+YakumaKuramy@users.noreply.github.com>
2023-09-11 02:09:30 -0300

    Update: Criação da box
     Criação da box e sua estrutura base, pode/deve ser revisado mais tarde

---
+++ world/object_scripts/box.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+
+@onready var raycast = $raycast
+
+@export var collid: int
+
+var grid_size := 16
+
+var inputs := {
+	"ui_up" : Vector2.UP,
+	"ui_down" : Vector2.DOWN,
+	"ui_left" : Vector2.LEFT,
+	"ui_right" : Vector2.RIGHT
+}
+
+func move(dir):
+	var _tween = create_tween()
+	var vector_pos = inputs[dir] * grid_size
+	raycast.force_raycast_update()
+	if raycast.is_colliding():
+		_tween.tween_property(self, "position", position + vector_pos * grid_size, 0.2)
+		return true
+	return false
