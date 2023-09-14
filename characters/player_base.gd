fc018daaeb3166d4e56f45d6cb361dccd7637b6b
FallenAngelDev <87909641+FallenAngelDev@users.noreply.github.com>
2023-09-14 16:35:06 -0300

    fix correção da colisão da flecha
    bug que tinha a chance do escudeiro morrer para a flecha, corrigido

--- characters/player_base.gd
+++ characters/player_base.gd
@@ -27,7 +27,8 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body as TileMap:
 		GlobalSignals.game_over.emit()
 		return
-	if body.get_parent().name == "Arrow":
+	print(body.collision_layer)
+	if body.collision_layer == 4:
 		if heavy:
 			body.get_parent().queue_free()
 			return
@@ -37,6 +38,5 @@ func _death() -> void:
 	active = false
 	$Animations.play("death")
 	await $Animations.animation_finished
-	print("game over")
 	SceneTransition._death_event()
 
