968304b12d37c2e78d0cc8f926cb9632c45738e6
FallenAngelDev <87909641+FallenAngelDev@users.noreply.github.com>
2023-09-15 21:25:58 -0300

    ajustes

--- world/object_scripts/wood_box.gd
+++ world/object_scripts/wood_box.gd
@@ -9,6 +9,7 @@ func _on_area_entered(area: Area2D) -> void:
 	if area.get_parent().name != "Player1":
 		return
 	current_position = self.position
+	$PushSound.play()
 	var move_box = create_tween()
 	move_box.tween_property(self,"position",
 		self.position + 16 * PlayerManager.current_direction,
