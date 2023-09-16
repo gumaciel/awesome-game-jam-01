c4de6222da85d1f789a8180e30932ff0bf575d4d
Gustavo Maciel <gustamaciel2012@hotmail.com>
2023-09-16 12:05:31 -0300

    fix flip

--- scenes/levels/grail_room.gd
+++ scenes/levels/grail_room.gd
@@ -8,16 +8,16 @@ var finishable := true
 func _on_areas_complete() -> void:
 	if finishable:
 		$AnimationPlayer.play("win")
-		if %Player1.active:
-			player_active = %Player1
+		if player_1.active:
+			player_active = player_1
 		else:
-			player_active = %Player2
+			player_active = player_2
 		player_active.active = false
 		flip()
 
 func flip() -> void:
-	%Player1/WinningSprite2D.flip_h = false
-	%Player2/WinningSprite2D.flip_h = true
+	player_1.get_node("WinningSprite2D").flip_h = player_1.position < player_2.position
+	player_2.get_node("WinningSprite2D").flip_h = player_1.position > player_2.position
 
 func finish() -> void:
 	finishable = false
