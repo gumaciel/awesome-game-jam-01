2cf0d36851073a15754e8a17498b00d49b0f9a01
Gustavo Maciel <gustamaciel2012@hotmail.com>
2023-09-16 21:19:45 -0300

    Update ending.gd

--- scenes/ending/ending.gd
+++ scenes/ending/ending.gd
@@ -2,4 +2,4 @@ extends Control
 
 func _on_animation_player_animation_finished(anim_name : StringName):
 	if anim_name == "load":
-		SceneTransition.change_scene_to_packed(null)
+		SceneTransition.change_scene_to_packed(load("res://scenes/main/main.tscn"))
